c191bc2abb8a820f6aa2dc5b5156a63cfb41d677
KEERTAN KUMAR <keertan2005@gmail.com>
2025-11-03 03:23:37 +0000

    Added current file(s)

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "keertan";
+
+//console.log(typeof score);
+//console.log(typeof(score)); //this type is known as method
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN{type is Number though}
+// true => 1; false => 0
+
+let isLoggedIn = "";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true & 0 => false
+// "" => false & "keertan" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
