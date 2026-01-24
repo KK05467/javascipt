97311a3e39fa7a6ed089ac426db1fd7c02e8d4c6
Keertan <keertan2005@gmail.com>
2026-01-25 00:30:44 +0530

    dates in js

---
+++ 01_basics/07_dates_injs.js
@@ -0,0 +1,33 @@
+//Dates
+
+let myDate = new Date()
+//console.log(myDate.toString());
+
+// console.log(myDate.toDateString());
+
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);   //output is Object
+
+
+// let myCreatedDate = new Date(2025, 0, 23)   //month starts from 0 in js
+
+// let myCreatedDate = new Date(2025, 0, 23,3, 5)
+
+let myCreatedDate = new Date("2025-01-14")
+// console.log(myCreatedDate.toLocaleString());
+
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+
+// console.log(Math.floor(Date.now()/1000));
+
+newDate.toLocaleString('default', {
+    weekday: "long"
+})
+
+
+
