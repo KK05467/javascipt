c6d92784516f8f7243db4a7a38449705527f04cf
Keertan <keertan2005@gmail.com>
2026-01-31 19:06:43 +0530

    arrow

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -19,14 +19,51 @@ const user = {
 
 //whenever you are referring your current context (here, the opening braces of user and closing braces of user), we user "this" keyword 
 
-function chai(){
+// function chai(){
 
+//     let username = "keertan"
+//     console.log(this.username);
+// }
+
+// chai()
+
+
+//it signifies that this keyword can only be used in the scope of object not function
+
+// const chai = function () {
+//     let username = "keertan"
+//     console.log(this.username);
+// }
+
+const chai = () => {
     let username = "keertan"
-    console.log(this.username);
+    console.log(this);
 }
 
 chai()
 
 
-//it signifies that this keyword can only be used in the scope of object not function
+const addTwo = (num1, num2) => {
+    return num1 + num2
+}
+
+//The above can also be written as
+
+// const addTwonum = (num1, num2) => num1 + num2  // implicit return
+
+const addTwonum = (num1, num2) => (num1 + num2)  //implicit return
+//If we use {num1.....} instead of (num1.....) then we have to use return keyword
+
+
+//Explicit return is when we use return keyword
+
+// console.log(addTwo(3,4))
+
+// console.log(addTwonum(2,5))
+
+
+// const myArray = [2, 4, 5, 3, 8]
+
+// myArray.forEach()
+
 
