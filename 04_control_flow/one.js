276382c59cd6f0a6138886088ec8b6499f3b6319
Keertan <keertan2005@gmail.com>
2026-02-01 12:23:01 +0530

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,27 @@
+//if
+
+const isUserloggedIn = true
+
+const temperature = 41
+
+// if (temperature < 50){
+//     console.log("Temperature is less than 50")
+// }
+
+// else{
+//     console.log("Temperature is greater than 50")
+// }
+
+// const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`)
+// }
+
+
+//console.log(`User power: ${power}`)  //Out of scope
+
+const balance = 1000
+
+if(balance > 500) console.log("test");
