e0e174c8979df6468d545350af2b4f50c2d477e5
Keertan <keertan2005@gmail.com>
2026-02-02 21:40:50 +0530

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,18 @@
+//while loop
+
+
+let index = 0
+while (index<10){
+    console.log(`Value of index is ${index}`);
+    index+=2
+}
+
+
+//do while loop
+
+
+let score = 11
+do{
+    console.log(`Score is ${score}`);
+    score++;
+} while(score<10)
