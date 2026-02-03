f326822a9500be0f6aaf136c33e3fc640b5ba586
Keertan <keertan2005@gmail.com>
2026-02-03 23:52:03 +0530

    reduce

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -12,3 +12,30 @@ const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
 
 
 console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: 'js Course',
+        price: 2999
+    },
+    {
+        itemName: 'py course',
+        price: 999
+
+    },
+    {
+        itemName: 'mobile dev course',
+        price: 5999
+    },
+    {
+        itemName: 'data science course',
+        price: 12999
+    }
+
+]
+
+
+const priceTopay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceTopay);
+
