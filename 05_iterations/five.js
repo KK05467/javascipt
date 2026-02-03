24abd6f74d01197cbb1b95b752652f4abfc5a33c
Keertan <keertan2005@gmail.com>
2026-02-03 23:13:13 +0530

    for each

---
+++ 05_iterations/five.js
@@ -0,0 +1,43 @@
+//for each loop
+
+const coding = ['js', 'ruby', 'python', 'cpp', 'javascript']
+
+// coding.forEach( function (item) {
+//     console.log(item)
+// } )
+
+// coding.forEach( (item) => {
+//     console.log(item)
+// })
+
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+coding.forEach( (item, index, arr) => {
+    console.log(item, index, arr)
+})
+
+
+const myCoding = [
+    {
+        languageName: 'javascript',
+        languageFileName: 'js'
+    },
+    {
+        languagename: 'java',
+        languageFileName: 'java'
+    },
+    {
+        languageName: 'python',
+        languageFileName: 'py'
+    }
+]
+
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName);
+})
